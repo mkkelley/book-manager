a47c85481d74e8fb3e6dc0b121a71e9932d1cf88
Michael Kelley <michael@minthe.net>
2020-09-10 00:41:21 -0500

    track DB operations

--- src/main/java/net/minthe/bookmanager/repositories/BookTagRepository.java
+++ src/main/java/net/minthe/bookmanager/repositories/BookTagRepository.java
@@ -3,7 +3,9 @@ package net.minthe.bookmanager.repositories;
 import net.minthe.bookmanager.models.BookTag;
 import net.minthe.bookmanager.models.BookTagId;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookTagRepository extends CrudRepository<BookTag, BookTagId> {
 
 }
--- src/main/java/net/minthe/bookmanager/repositories/TagRepository.java
+++ src/main/java/net/minthe/bookmanager/repositories/TagRepository.java
@@ -3,7 +3,10 @@ package net.minthe.bookmanager.repositories;
 import java.util.List;
 import net.minthe.bookmanager.models.Tag;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepository extends CrudRepository<Tag, Long> {
+
   List<Tag> findByTagIn(Iterable<String> tags);
 }
