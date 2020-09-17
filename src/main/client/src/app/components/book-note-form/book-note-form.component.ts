c26dcb64022823ab872d59fa67ac33fc79b1c7e3
Michael Kelley <michael@minthe.net>
2020-09-16 19:28:41 -0500

    Clear form when submitting book note
    BM-38

--- src/main/client/src/app/components/book-note-form/book-note-form.component.ts
+++ src/main/client/src/app/components/book-note-form/book-note-form.component.ts
@@ -1,7 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { BookNote } from '../../models/book-note';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-note-form',
@@ -10,7 +9,6 @@ import { Observable } from 'rxjs';
 })
 export class BookNoteFormComponent implements OnInit {
   @Output() note = new EventEmitter<Partial<BookNote>>();
-  @Input() clear: Observable<any>;
   public form: FormGroup;
 
   constructor() {}
@@ -19,18 +17,12 @@ export class BookNoteFormComponent implements OnInit {
     this.form = new FormGroup({
       notes: new FormControl(''),
     });
-    if (this.clear) {
-      this.clear.subscribe(() => {
-        this.form = new FormGroup({
-          notes: new FormControl(''),
-        });
-      });
-    }
   }
 
   submit(): void {
     this.note.emit({
       notes: this.form.controls.notes.value,
     });
+    this.form.controls.notes.setValue('');
   }
 }
